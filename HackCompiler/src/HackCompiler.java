c93bce86fba64d49f72aa6bd0a72d7b95bf571d7
doughnuttouch <67760292+doughnuttouch@users.noreply.github.com>
2020-08-13 11:36:23 -0400

    create foundation

---
+++ HackCompiler/src/HackCompiler.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class HackCompiler
+{
+	static String className;
+	
+	public static void main(String[] args)
+	{
+		String codeFinal = "";
+		String strfile;
+		File file = new File(args[0]);
+		
+		BufferedReader in;
+		strfile = file.toString();
+		if(file.isDirectory())
+		{
+			String[] dirContent = file.list();
+			for(String d : dirContent)
+			{
+				d = file.toString() + "\\" + d;
+				if(d.substring(d.length() - 5, d.length()).equals(".jack"))
+				{
+					in = new BufferedReader(new FileReader(new File(d)));
+					strfile = file.toString();
+					className = d.substring(d.lastIndexOf("\\") + 1, d.length() - 5);
+					if(codeFinal.isEmpty())
+						codeFinal = coder(parser(in));
+					else
+						codeFinal = codeFinal.concat(coder(parser(in)));
+				}
+			}
+		}
+		else
+		{
+			if(strfile.substring(strfile.length() - 3, strfile.length()).equals(".vm"))
+			{
+				in = new BufferedReader(new FileReader(file));
+				strfile = file.toString();
+				className = strfile.substring(strfile.lastIndexOf("\\") + 1, strfile.length() - 3);
+				
+				if(codeFinal.isEmpty())
+					codeFinal = coder(parser(in));
+				else
+					codeFinal = codeFinal.concat(coder(parser(in)));
+			}
+		}
+	}
+}
