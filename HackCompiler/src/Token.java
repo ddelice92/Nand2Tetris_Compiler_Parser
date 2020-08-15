06744377a78fc368d393f69ec70fb30abcef7384
doughnuttouch <67760292+doughnuttouch@users.noreply.github.com>
2020-08-15 10:07:57 -0400

    add token class, token test and start token method

---
+++ HackCompiler/src/Token.java
@@ -0,0 +1,17 @@
+
+public class Token
+{
+	String name, type;
+	
+	public Token(String name, String type)
+	{
+		this.name = name;
+		this.type = type;
+	}
+	
+	public String toString()
+	{
+		String string = "<" + this.type + ">" + this.name + "</" + this.type + ">\n";
+		return string;
+	}
+}
