b1159503f181a5754bb07f85f5cc50dadf0a4642
doughnuttouch <67760292+doughnuttouch@users.noreply.github.com>
2020-08-18 13:52:14 -0400

    update tokenizer method

--- HackCompiler/src/HackCompiler.java
+++ HackCompiler/src/HackCompiler.java
@@ -1,10 +1,9 @@
-//***********************
-//THIS LINE IS A GITHUB TEST
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class HackCompiler
 {
@@ -55,18 +54,54 @@ public class HackCompiler
 					codeFinal = parser(tokenizer(in));
 				else
 					codeFinal = codeFinal.concat(parser(tokenizer(in)));*/
+				
+				//this is to test tokening
+				Token[] tokArray = tokenizer(in);
+				for(Token t : tokArray)
+					codeFinal = codeFinal.concat(t.toString());
 			}
-			
-			//this is to test tokening
-			Token[] tokArray = tokenizer(in);
-			for(Token t : tokArray)
-				codeFinal = codeFinal.concat(t.toString());
 		}
 	}
 	
-	public static Token[] tokenizer(BufferedReader read)
+	public static Token[] tokenizer(BufferedReader read) throws IOException
 	{
-		ArrayList<char> chArr = new ArrayList<char>();
+		ArrayList<String> strArr = new ArrayList<String>();
+		char[] chArr;
+		String lineTemp = read.readLine();
+		String wordTemp = "";
+		boolean strLit = false;
 		
+		while(!lineTemp.equals(null))
+		{
+			chArr = lineTemp.toCharArray();
+			for(char c : chArr)
+			{
+				if(Character.toString(c).equals("\""))
+				{
+					if(strLit == false)
+						strLit = true;
+					else
+						strLit = false;
+				}
+				
+				if(strLit == true && !Character.toString(c).equals("\""))
+				{
+					wordTemp = wordTemp.concat(Character.toString(c));
+				}
+				else if(strLit == false && !Character.toString(c).equals("\"") &&
+						!Character.toString(c).isBlank())
+				{
+					wordTemp = wordTemp.concat(Character.toString(c));
+				}
+				
+				//if c is blank and strlit is false or if c is " and strlit is false, add to string array
+				if(strLit == false && (Character.toString(c).isBlank() || Character.toString(c).equals("\"")))
+				{
+					strArr.add(wordTemp);
+					wordTemp = "";
+				}
+			}
+			strArr.add(wordTemp);
+		}
 	}
 }
